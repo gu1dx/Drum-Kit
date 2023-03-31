0dfc85f4e2ce1ae8c1db8de45b3b6d072e8a39ce
YOUR NAME <guadybenitez@gmail.com>
2023-03-31 18:22:19 -0300

    added keydown event listener, to play drums with the keyboard as well

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Detecting Button Press
+
 document.querySelectorAll(".drum").forEach(button => {
    button.addEventListener("click", handleClick);
    
@@ -6,9 +8,25 @@ document.querySelectorAll(".drum").forEach(button => {
 function handleClick(){
    
    var buttonInnerHTML = this.innerHTML;
-   
-   switch (buttonInnerHTML) {
+
+   makeSound(buttonInnerHTML);
+         
+}
+
+//Detecting Keyboard Press
+
+document.addEventListener("keydown", function(event) {
+   makeSound(event.key);
+});
+
+
+function makeSound(key){
+   switch (key) {
       case "w":
+         var kick = new Audio('sounds/kick-bass.mp3');
+         kick.play();      
+         break;
+      case "W":
          var kick = new Audio('sounds/kick-bass.mp3');
          kick.play();      
          break;
@@ -16,7 +34,15 @@ function handleClick(){
          var snare = new Audio('sounds/snare.mp3');
          snare.play(); 
       break;
+      case "A":
+         var snare = new Audio('sounds/snare.mp3');
+         snare.play(); 
+      break;
       case "s":
+         var tom1 = new Audio('sounds/tom-1.mp3');
+         tom1.play(); 
+      break;
+      case "S":
          var tom1 = new Audio('sounds/tom-1.mp3');
          tom1.play(); 
       break;
@@ -24,7 +50,15 @@ function handleClick(){
          var tom2 = new Audio('sounds/tom-2.mp3');
          tom2.play(); 
       break;
+      case "D":
+         var tom2 = new Audio('sounds/tom-2.mp3');
+         tom2.play(); 
+      break;
       case "j":
+         var tom3 = new Audio('sounds/tom-3.mp3');
+         tom3.play(); 
+      break;
+      case "J":
          var tom3 = new Audio('sounds/tom-3.mp3');
          tom3.play(); 
       break;
@@ -32,7 +66,15 @@ function handleClick(){
          var tom4 = new Audio('sounds/tom-4.mp3');
          tom4.play(); 
       break;
+      case "K":
+         var tom4 = new Audio('sounds/tom-4.mp3');
+         tom4.play(); 
+      break;
       case "l":
+         var crash = new Audio('sounds/crash.mp3');
+         crash.play(); 
+      break;
+      case "L":
          var crash = new Audio('sounds/crash.mp3');
          crash.play(); 
       break;
@@ -41,6 +83,4 @@ function handleClick(){
          console.log(buttonInnerHTML);
          break;
    }
-   
-   
 }
