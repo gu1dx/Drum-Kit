892d6e86e2edd523b34af7b31f5a7180c51ffe69
gu1dx <guadybenitez@gmail.com>
2023-04-13 08:51:29 -0300

    finished website

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function handleClick(){
    var buttonInnerHTML = this.innerHTML;
 
    makeSound(buttonInnerHTML);
+   buttonAnimation(buttonInnerHTML);
          
 }
 
@@ -17,6 +18,7 @@ function handleClick(){
 
 document.addEventListener("keydown", function(event) {
    makeSound(event.key);
+   buttonAnimation(event.key);
 });
 
 
@@ -83,4 +85,14 @@ function makeSound(key){
          console.log(buttonInnerHTML);
          break;
    }
+}
+
+function buttonAnimation(currentKey) {
+
+   var activeButton = document.querySelector("." + currentKey);
+   activeButton.classList.add("pressed");
+   setTimeout(function(){
+      activeButton.classList.remove("pressed");
+   }, 100);
+   
 }
